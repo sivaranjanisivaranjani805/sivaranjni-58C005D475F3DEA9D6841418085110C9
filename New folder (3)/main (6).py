4a8ad1e48efbda4245722de71f7d2357da1531ca
sivaranjanisivaranjani805 <144577782+sivaranjanisivaranjani805@users.noreply.github.com>
2023-10-23 09:32:49 +0530

    Add files via upload

---
+++ New folder (3)/main (6).py
@@ -0,0 +1,15 @@
+# initialize
+a = []
+
+# create the table (name, age, job)
+a.append(["Nick", 30, "Doctor"])
+a.append(["John",  8, "Student"])
+a.append(["Paul", 22, "Car Dealer"])
+a.append(["Mark", 66, "Retired"])    
+
+# sort the table by age
+import operator
+a.sort(key=operator.itemgetter(1))    
+
+# print the table
+print(a)
